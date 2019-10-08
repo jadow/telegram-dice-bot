cba971e126f36618782c300ef4dd1c8af39de1b8
Foo Yong Jie <yongjie.foo@shopee.com>
2019-10-08 14:05:48 +0800

    fix message

--- main.go
+++ main.go
@@ -85,7 +85,7 @@ func getDiceMessage(name string, random int) string {
 }
 
 func getDiceRollMessage(name string, dice string, random int) string {
-	return fmt.Sprintf("%s rolled a %s dice, ", name, dice) + getDiceMessage(name, random)
+	return fmt.Sprintf("%s rolled a %s, ", name, dice) + getDiceMessage(name, random)
 }
 
 func getQuery(update tgbotapi.Update) tgbotapi.InlineConfig {
